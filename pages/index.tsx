908bd3948077530dc16ae0e4fafd1489035805b9
mitsuhiro <papu.m0917@icloud.com>
2022-01-13 21:35:41 +0900

    textステートの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+    const [text, setText] = useState('');
+
+
     return (
         <div className={styles.container}>
             <Head>
@@ -12,43 +16,19 @@ export default function Home() {
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://nextjs.org">Next.js+typescript!</a>
-                </h1>
-
-                <p className={styles.description}>
-                    Get started by editing{' '}
-                    <code className={styles.code}>pages/index.js</code>
-                </p>
-
-                <div className={styles.grid}>
-                    <a href="https://nextjs.org/docs" className={styles.card}>
-                        <h2>Documentation &rarr;</h2>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
-
-                    <a href="https://nextjs.org/learn" className={styles.card}>
-                        <h2>Learn &rarr;</h2>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
-
-                    <a
-                        href="https://github.com/vercel/next.js/tree/canary/examples"
-                        className={styles.card}
-                    >
-                        <h2>Examples &rarr;</h2>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className={styles.card}
-                    >
-                        <h2>Deploy &rarr;</h2>
-                        <p>
-                            Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
+                <h1 className={styles.title}>ToDo App</h1>
+                <div>
+                    <form onSubmit={(e) => e.preventDefault()}>
+                        <input type="text"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        />
+                        <input
+                            type="submit"
+                            value="追加"
+                            onSubmit={(e) => e.preventDefault()}
+                        />
+                    </form>
                 </div>
             </main>
 
