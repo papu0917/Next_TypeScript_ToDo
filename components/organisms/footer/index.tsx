c65e03421c8f3ca73ecb70c8c03126e85cb98290
mitsuhiro <papu.m0917@icloud.com>
2022-02-19 22:26:58 +0900

    インデント修正

--- components/organisms/footer/index.tsx
+++ components/organisms/footer/index.tsx
@@ -2,18 +2,23 @@ import { NextImage } from "~/components/atoms/next-image";
 import { FooterRoot, SpanImageWrapper } from "./style";
 
 export const Footer = () => {
-    return (
-        <FooterRoot>
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{' '}
-                <SpanImageWrapper>
-                    <NextImage src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                </SpanImageWrapper>
-            </a>
-        </FooterRoot>
-    );
-}
+  return (
+    <FooterRoot>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Powered by{" "}
+        <SpanImageWrapper>
+          <NextImage
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            width={72}
+            height={16}
+          />
+        </SpanImageWrapper>
+      </a>
+    </FooterRoot>
+  );
+};
